78c0bbe996109868caf23ed1d6c0020199e628ed
Sean Ho <github@seanho.com>
2018-07-13 17:08:18 -0700

    add fmt.proc_time

---
+++ R/proc_time.r
@@ -0,0 +1,25 @@
+#' Pretty-print a proc_time object
+#'
+#' @param pt A proc_time object, e.g., from system.time()
+#'
+#' @return A formatted string.
+#' @export
+#'
+#' @examples
+#' fmt.proc_time(system.time(rnorm(1e7)))
+fmt.proc_time <- function(pt) {
+  library(tidyverse)
+  library(lubridate)
+
+  pt_per <- pt %>%
+    as.list() %>%
+    .$elapsed %>%
+    seconds_to_period()
+
+  c("day", "hour", "minute", "second") %>%
+    set_names() %>%
+    map(do.call, list(pt_per)) %>%
+    .[.>0] %>%
+    imap(~sprintf("%d %ss", floor(.x), .y)) %>%
+    paste(collapse = " ")
+}
