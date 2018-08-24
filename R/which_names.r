9e5983e8386c7b7d3addfa7570e5da983263b776
Sean Ho <github@seanho.com>
2018-08-23 21:25:51 -0700

    update docs

--- R/which_names.r
+++ R/which_names.r
@@ -1,15 +1,13 @@
-# Find indices of TRUE entries in an array
-
 #' Find indices of TRUE entries in an array
-#' 
+#'
 #' `base::which(arr.ind = TRUE)` returns numeric indices of TRUE entries
 #' in an array.
 #' This wrapper uses `dimnames` to return pairs of row/column names.
 #'
 #' @param .a array (could be matrix)
 #' @param dimnames list of character vectors.
-#'   E.g., if `.a` is a matrix, then `length(dimnames) == 2`, 
-#'   `dimnames[[1]]` is a vector of row names, and 
+#'   E.g., if `.a` is a matrix, then `length(dimnames) == 2`,
+#'   `dimnames[[1]]` is a vector of row names, and
 #'   `dimnames[[2]]` is a vector of column names.
 #' @param ... Additional options passed to `base::which()``.
 #'
@@ -19,13 +17,13 @@
 #' @export
 #'
 #' @examples
+#' which_names(mtcars[1:6, c("vs", "am")] == 1)
 which_names <- function(.a, dimnames = NULL, ...) {
   if (!is.null(dimnames)) {
     dimnames(.a) <- dimnames
   }
-  which(.a, arr.ind = TRUE, ...) %>% 
-    dplyr::as_tibble() %>% 
-    purrr::map2(seq_along(.), ~dimnames(.a)[[.y]][.x]) %>% 
+  which(.a, arr.ind = TRUE, ...) %>%
+    dplyr::as_tibble() %>%
+    purrr::map2(seq_along(.), ~dimnames(.a)[[.y]][.x]) %>%
     dplyr::as_tibble()
 }
-
