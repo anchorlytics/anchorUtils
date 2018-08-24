6a83502bb525b68f91eb9cc297ec654d6df39c87
Sean Ho <github@seanho.com>
2018-08-24 01:16:43 -0700

    dummy_code: reduce reliance on dplyr

--- R/data_prep.r
+++ R/data_prep.r
@@ -16,14 +16,14 @@
 #' The new indicator variables are appended to the end of the variable list,
 #' and the original factors are removed.
 #'
-#' @param .data data frame or tibble
+#' @param .data data frame
 #' @param ... One or more unquoted expressions separated by commas,
 #'   as passed to [dplyr::select()].
 #'   If omitted, all variables are considered.
 #'
-#' @return A tibble with selected factors converted to logical dummy variables
+#' @return data frame with selected factors converted to logical dummy variables
 #'
-#' @importFrom dplyr %>% quos everything bind_cols select select_if one_of mutate_all as_tibble
+#' @importFrom dplyr quos everything select select_if one_of
 #' @importFrom stats model.frame model.matrix contrasts
 #' @family data munging
 #' @author Sean Ho <anchor@seanho.com>
@@ -31,7 +31,6 @@
 #' @examples
 #' library(dplyr)
 #' mutate_at(mtcars, c("cyl", "gear"), as.factor) %>% dummy_code()
-#'
 #' mutate_at(mtcars, c("cyl", "gear"), as.factor) %>% dummy_code(-cyl)
 dummy_code <- function(.data, ...) {
   sel = quos(...)
@@ -40,34 +39,42 @@ dummy_code <- function(.data, ...) {
   }
 
   noms <-
-    .data %>%
-    select(!!!sel) %>%
-    select_if(~is.factor(.) & !is.ordered(.)) %>%
-    select_if(~length(na.omit(unique(.))) > 2) %>%
-    names()
-
-  .data %>%
-    select(-one_of(noms)) %>%
-    bind_cols(
-      select(.data, one_of(noms)) %>%
-        stats::model.frame(~., ., na.action = "na.pass") %>%
-        stats::model.matrix(
-          ~.-1, data = .,
-          contrasts.arg = lapply(., stats::contrasts, contrasts = FALSE)
-        ) %>%
-        as_tibble() %>%
-        mutate_all(as.logical)
+    names(
+      select_if(
+        select_if(
+          select(.data, !!!sel),
+          ~is.factor(.) & !is.ordered(.)
+        ),
+        ~length(na.omit(unique(.))) > 2
+      )
     )
+
+  nom_frame <-
+    model.frame(~., select(.data, one_of(noms)), na.action = "na.pass")
+
+  cbind(
+    select(.data, -one_of(noms)),
+    sapply(
+      as.data.frame(
+        model.matrix(
+          ~.-1, data = nom_frame,
+          contrasts.arg = lapply(nom_frame, contrasts, contrasts = FALSE)
+        )
+      ),
+      as.logical
+    )
+  )
 }
 
-#' Compress ordinal factors with too many levels
+#' Compress integer ordinal factors with too many levels
 #'
 #' Polychoric correlation routines often limit the number of levels an
 #' ordinal (ordered categorical) factor can have.
 #' This function is like [forcats::fct_collapse()] but preserves the ordering
 #' of levels.
+#' Modified factors are releveled to 1:n.
 #'
-#' @param .data data frame with proper column types
+#' @param .data data frame with ordinal factors coded as integers
 #' @param maxlev apply level collapsing only if `nlevels` is greater than this
 #' @param newlev if collapsing levels, how many levels should the factor have.
 #'   Default is `maxlev`.
