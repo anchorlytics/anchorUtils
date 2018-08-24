e0111bba114a29e41c8e732d3004eba116f2d00a
Sean Ho <github@seanho.com>
2018-08-24 00:14:31 -0700

    doc for overall package

---
+++ R/anchorUtils.r
@@ -0,0 +1,17 @@
+#' anchorUtils: In-house utility functions for Anchorlytics Consulting
+#'
+#' This package is a dump-bin of miscellaneous helper functions accumulated
+#' over the course of several projects.
+#'
+#' @section Saving/loading data:
+#' Functions to quickly save/load a set of R data structures.
+#'
+#' @section Data munging:
+#' Functions to help process datasets in preparation for analysis.
+#'
+#' @section Mplus helpers:
+#' Functions to help format strings for Mplus input syntax.
+#'
+#' @docType package
+#' @name anchorUtils
+NULL
