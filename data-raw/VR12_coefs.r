55e7244d5d33a772c3c7b0fd29107624e2237395
Sean Ho <git@seanho.com>
2019-01-23 11:46:25 -0800

    use namespaces in data generation code

--- data-raw/VR12_coefs.r
+++ data-raw/VR12_coefs.r
@@ -8,13 +8,13 @@ library(usethis)
 VR12_coefs <-
   # system.file("data-raw", "VR12_coefs.csv", package = "anchorUtils") %>%
   file.path("data-raw", "VR12_coefs.csv") %>%
-  read_csv(col_types = "ccddddddddddddd") %>%
-  nest(-Domain, -Mode) %>%
-  mutate_at("data", ~map(., as_vector)) %>%
-  spread(Domain, data) %>%
-  nest(-Mode) %>%
-  spread(Mode, data) %>%
-  flatten() %>%
-  map(flatten)
+  readr::read_csv(col_types = "ccddddddddddddd") %>%
+  tidyr::nest(-Domain, -Mode) %>%
+  dplyr::mutate_at("data", ~purrr::map(., purrr::as_vector)) %>%
+  tidyr::spread(Domain, data) %>%
+  tidyr::nest(-Mode) %>%
+  tidyr::spread(Mode, data) %>%
+  purrr::flatten() %>%
+  purrr::map(purrr::flatten)
 
 usethis::use_data(VR12_coefs, internal = TRUE, overwrite = TRUE)
